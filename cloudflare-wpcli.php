a96399137a289f89da12f2a7227c9d1f9bc302c8
Brandon Hubbard <bkhubbard@gmail.com>
2016-08-17 14:45:14 -0700

    wpcli support

--- cloudflare-wpcli.php
+++ cloudflare-wpcli.php
@@ -9,6 +9,15 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 
-/*
-To be created, wpcli support for CloudFlare API Calls.
-*/
+/**
+ * CloudFlare_WP_CLI_Commands class.
+ *
+ * @extends WP_CLI_Command
+ */
+class CloudFlare_WP_CLI_Commands extends WP_CLI_Command {
+
+	/* TBD: CLOUDFLARE WP-CLI COMMANDS */
+
+}
+
+WP_CLI::add_command( 'cloudflare', 'CloudFlare_WP_CLI_Commands' );
