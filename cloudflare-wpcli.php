d9f11d2ad91bcce5b5f1c58e8e4fdc286aa4a3b4
Brandon Hubbard <bkhubbard@gmail.com>
2016-08-17 14:42:25 -0700

    cli file

---
+++ cloudflare-wpcli.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CloudFlare WPCLI
+ *
+ * @package wp-cloudflare-wpcli
+ */
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+
+/*
+To be created, wpcli support for CloudFlare API Calls.
+*/
